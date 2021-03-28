170e082925fc1cc88bbf41509b9363d2503edc89
m-babita <babita.majumdar.1@gmail.com>
2021-03-28 17:56:21 +0530

    Added a filter in todo list

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterSelect =document.querySelector(".todo-filter");
 
 //event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterSelect.addEventListener("click",filterTodo);
 
 //functions
 function addTodo(event){
@@ -60,3 +62,32 @@ function deleteCheck(e){
           todo.classList.toggle("completed");
      }
 }
+
+function filterTodo(e) {
+     const todos = todoList.childNodes;
+     todos.forEach(function(todo) {
+          switch (e.target.value) {
+               case "all": 
+                 todo.style.display="flex";
+                    break;
+               case "completed":
+                    if(todo.classList.contains("completed")){
+                         todo.style.display="flex";
+                    }    
+                         else{
+                              todo.style.display="none";
+                         }
+                    break;
+               case "uncompleted":
+                    if(!todo.classList.contains("completed")) {
+                         todo.style.display= "flex";
+                    }
+                    else{
+                         todo.style.display= "none";
+                    }
+                    break;
+                    
+          }
+     });
+}
+
